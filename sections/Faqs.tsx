aa5bd6e9cd31511231e4054d31b22a0d8fcf41bb
Muhammad Zohaib Irshad <zohaibirshad678@gmail.com>
2025-09-05 17:02:11 +0500

    navbar-animation

--- sections/Faqs.tsx
+++ sections/Faqs.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Tag from '@/components/Tag'
+import { twMerge } from 'tailwind-merge'
 
 
 const faqs = [
@@ -27,16 +28,19 @@ const faqs = [
 
 
 const Faqs = () => {
+    const selectedIndex=0;
     return (
-        <section>
+        <section className='py-24'>
             <div className="container">
-                <Tag>Faqs</Tag>
-                <h2>Questions? We&apos;ve got <span>answers</span></h2>
-                <div>
-                    {faqs.map((i) =>
-                        <div key={i.question}>
-                            <div>
-                                <h3>{i.question}</h3>
+                <div className='flex justify-center'>
+                    <Tag>Faqs</Tag>
+                </div>
+                <h2 className='text-6xl font-medium mt-6 text-center max-w-xl mx-auto'>Questions? We&apos;ve got <span className='text-lime-400'>answers</span></h2>
+                <div className='mt-12 flex flex-col gap-6 max-w-xl mx-auto'>
+                    {faqs.map((i,faqIndex) =>
+                        <div key={i.question} className='bg-neutral-900 border-2 border-white/10 rounded-2xl p-5'>
+                            <div className='flex justify-between items-center'>
+                                <h3 className='font-medium'>{i.question}</h3>
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     width="24"
@@ -47,12 +51,15 @@ const Faqs = () => {
                                     strokeWidth="2"
                                     strokeLinecap="round"
                                     strokeLinejoin="round"
-                                    className="feather feather-plus-circle"
+                                    className={twMerge("feather feather-plus-circle text-lime-400 flex-shrink-0",selectedIndex===faqIndex && 'rotate-45')}
                                 >
                                     <circle cx="12" cy="12" r="10" />
                                     <line x1="12" y1="8" x2="12" y2="16" />
                                     <line x1="8" y1="12" x2="16" y2="12" />
                                 </svg>
+                            </div>
+                            <div className={twMerge('mt-6',selectedIndex !== faqIndex && 'hidden')}>
+                                                            <p className='text-white/50'>{i.answer}</p>
                             </div>
                         </div>
                     )}
--- sections/Navbar.tsx
+++ sections/Navbar.tsx
@@ -1,5 +1,7 @@
+"use client";
 import Button from "@/components/Button";
-
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 const navLinks = [
     { label: 'Home', href: '#' },
@@ -9,71 +11,88 @@ const navLinks = [
 ]
 
 export default function Navbar() {
+    const [isOpen, setisOpen] = useState(false);
     return (
-        <section className="px-4 py-4 lg:py-8 lg:p-16">
+        <section className="px-4 py-4 lg:py-8 lg:p-16 sticky top-0 z-50">
             <div className="container">
-                <div className="flex items-center justify-between border border-white/15 rounded-full p-2 px-4 md:pr-2">
+                <div className="bg-neutral-900/70 backdrop-blur border border-white/15 rounded-[27px]">
+                    <div className="flex items-center justify-between p-2 px-4 md:pr-2">
 
-                    {/* Left: Logo */}
-                    <div>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width={24}
-                            height={24}
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth={2}
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            className="feather feather-codesandbox text-lime-400 h-9 w-auto"
-                        >
-                            <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z" />
-                            <polyline points="7.5 4.21 12 6.81 16.5 4.21" />
-                            <polyline points="7.5 19.79 7.5 14.6 3 12" />
-                            <polyline points="21 12 16.5 14.6 16.5 19.79" />
-                            <polyline points="3.27 6.96 12 12.01 20.73 6.96" />
-                            <line x1={12} y1={22.08} x2={12} y2={12} />
-                        </svg>
+                        {/* Left: Logo */}
+                        <div>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width={24}
+                                height={24}
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth={2}
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                className="feather feather-codesandbox text-lime-400 h-9 w-auto"
+
+                            >
+                                <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z" />
+                                <polyline points="7.5 4.21 12 6.81 16.5 4.21" />
+                                <polyline points="7.5 19.79 7.5 14.6 3 12" />
+                                <polyline points="21 12 16.5 14.6 16.5 19.79" />
+                                <polyline points="3.27 6.96 12 12.01 20.73 6.96" />
+                                <line x1={12} y1={22.08} x2={12} y2={12} />
+                                <line x1={12} y1={22.08} x2={12} y2={12} />
+                                <line x1={12} y1={22.08} x2={12} y2={12} />
+
+                            </svg>
+                        </div>
+
+                        {/* Middle: Nav Links */}
+                        <nav className="hidden lg:flex gap-6 font-medium">
+                            {navLinks.map((i) => (
+                                <a href={i.href} key={i.label}>
+                                    {i.label}
+                                </a>
+                            ))}
+                        </nav>
+
+                        {/* Right: Menu + Buttons */}
+                        <div className="flex items-center gap-4">
+                            {/* Mobile menu */}
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width={24}
+                                height={24}
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth={2}
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                onClick={() => setisOpen(!isOpen)}
+                                className="feather feather-menu md:hidden cursor-pointer"
+                            >
+                                <line x1={3} y1={12} x2={21} y2={12} className={twMerge('origin-center transition-transform duration-300', isOpen && 'rotate-45 translate-y-0')} />
+                                <line x1={3} y1={6} x2={21} y2={6} className={twMerge('origin-center transition-all duration-300', isOpen && 'opacity-0 rotate-180')} />
+                                <line x1={3} y1={18} x2={21} y2={18} className={twMerge('origin-center transition-transform duration-300', isOpen && '-rotate-45 ')} />
+                            </svg>
+
+                            {/* Desktop buttons */}
+                            <Button variant="secondary" className="hidden md:inline-flex items-center">
+                                Log In
+                            </Button>
+                            <Button variant="primary" className="hidden md:inline-flex items-center">
+                                Sign Up
+                            </Button>
+                        </div>
                     </div>
-
-                    {/* Middle: Nav Links */}
-                    <nav className="hidden lg:flex gap-6 font-medium">
-                        {navLinks.map((i) => (
-                            <a href={i.href} key={i.label}>
-                                {i.label}
-                            </a>
-                        ))}
-                    </nav>
-
-                    {/* Right: Menu + Buttons */}
-                    <div className="flex items-center gap-4">
-                        {/* Mobile menu */}
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width={24}
-                            height={24}
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth={2}
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            className="feather feather-menu md:hidden"
-                        >
-                            <line x1={3} y1={12} x2={21} y2={12} />
-                            <line x1={3} y1={6} x2={21} y2={6} />
-                            <line x1={3} y1={18} x2={21} y2={18} />
-                        </svg>
-
-                        {/* Desktop buttons */}
-                        <Button variant="secondary" className="hidden md:inline-flex items-center">
-                            Log In
-                        </Button>
-                        <Button variant="primary" className="hidden md:inline-flex items-center">
-                            Sign Up
-                        </Button>
-                    </div>
+                    {isOpen && (
+                        <div className="flex flex-col items-center gap-4 py-4">
+                            {navLinks.map((i) =>
+                                <a href={i.href} key={i.label} className="">{i.label}</a>
+                            )}
+                            <Button variant="primary">Log In</Button>
+                            <Button variant="secondary">Sign Up</Button>
+                        </div>
+                    )}
                 </div>
             </div>
         </section>
